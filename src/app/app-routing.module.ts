ff1552962e53accc7a513458c300f4c8586258f6
ayang223 <a.yang223@gmail.com>
2023-07-17 15:35:27 -0700

    feat: add auth0 to project with basic navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadChildren: () => import('./features/home/home.module').then((m) => m.HomeModule),
+    },
+    {
+        path: 'dashboard',
+        loadChildren: () => import('./features/dashboard/dashboard.module').then((m) => m.DashboardModule),
+        canActivate: [AuthGuard],
+    },
+    {
+        path: 'callback',
+        loadChildren: () => import('./features/callback/callback.module').then((m) => m.CallbackModule),
+    },
+    // TODO: add not found module
+    //   {
+    //     path: '**',
+    //     loadChildren: () =>
+    //       import('./features/not-found/not-found.module').then(
+    //         (m) => m.NotFoundModule
+    //       ),
+    //   }
+    // {
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {
+            scrollPositionRestoration: 'enabled',
+        }),
+    ],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
