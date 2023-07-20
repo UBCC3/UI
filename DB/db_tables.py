d5ab8837896cd81202c71549e5b5bdc834818241
Ury Segal <4493070+urysegal@users.noreply.github.com>
2023-07-20 11:38:20 -0700

    Some work on user management

---
+++ DB/db_tables.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Table
+from sqlalchemy.ext.declarative import declarative_base
+from DB.entity_names import USERS_TABLE_NAME, JOBS_TABLE_NAME, STRUCTURES_TABLE_NAME, \
+                            STRUCTURE_PROPERTIES_TABLE_NAME, JOB_TAGS_TABLE_NAME
+from DB.db_engine import db_engine
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __table__ = Table(USERS_TABLE_NAME, Base.metadata, autoload_with=db_engine.engine)
+
+
+class Job(Base):
+    __table__ = Table(JOBS_TABLE_NAME, Base.metadata, autoload_with=db_engine.engine)
+
+
+class Structure(Base):
+    __table__ = Table(STRUCTURES_TABLE_NAME, Base.metadata, autoload_with=db_engine.engine)
+
+
+class Structure_Property(Base):
+    __table__ = Table(STRUCTURE_PROPERTIES_TABLE_NAME, Base.metadata, autoload_with=db_engine.engine)
+
+
+class Job(Base):
+    __table__ = Table(JOB_TAGS_TABLE_NAME, Base.metadata, autoload_with=db_engine.engine)
