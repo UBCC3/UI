e5173de748861a03a782ae18e18b25ac6b4bc172
ayang223 <a.yang223@gmail.com>
2023-09-20 16:24:57 -0700

    refactor: added orderby ascending id order for getting calculation, basis set and methods

--- server/app/database/calculation_management.py
+++ server/app/database/calculation_management.py
@@ -3,6 +3,7 @@ from .db_engine import db_engine
 from .db_tables import Available_Basis_Sets, Available_Calculations, Available_Methods
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import asc
 
 from typing import List
 from ..models import (
@@ -14,20 +15,30 @@ from ..models import (
 
 def get_all_available_calculations() -> List[AvailableCalculationsModel]:
     with Session(db_engine.engine) as session:
-        available_calculations = session.query(Available_Calculations).all()
+        available_calculations = (
+            session.query(Available_Calculations)
+            .order_by(asc(Available_Calculations.id))
+            .all()
+        )
 
     return available_calculations
 
 
 def get_all_available_basis_sets() -> List[AvailableBasisSetsModel]:
     with Session(db_engine.engine) as session:
-        available_basis_sets = session.query(Available_Basis_Sets).all()
+        available_basis_sets = (
+            session.query(Available_Basis_Sets)
+            .order_by(asc(Available_Basis_Sets.id))
+            .all()
+        )
 
     return available_basis_sets
 
 
 def get_all_available_methods() -> List[AvailableMethodsModel]:
     with Session(db_engine.engine) as session:
-        available_methods = session.query(Available_Methods).all()
+        available_methods = (
+            session.query(Available_Methods).order_by(asc(Available_Methods.id)).all()
+        )
 
     return available_methods
