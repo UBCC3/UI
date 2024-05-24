baa29cd075d622acfb27f4d268cdcc2819ec8181
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-05-24 16:01:37 -0700

    SETUP .env file on the python api

--- server/app/database/db_engine.py
+++ server/app/database/db_engine.py
@@ -4,16 +4,20 @@ from pydantic import BaseModel, PrivateAttr
 from .entity_names import DB_NAME
 import logging
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from pathlib import Path
 
-
+dotenv_path = os.getcwd()+"/.env"
+load_dotenv(dotenv_path)
 class DB_Engine(BaseModel):
     _engine: Engine = PrivateAttr()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        host = "localhost"
-        db_username="postgres"
-        db_passwd = os.environ["PG_PASSWD"]
+        host = os.environ.get("RDS_HOST")
+        db_username= os.environ.get("RDS_USERNAME")
+        db_passwd = os.environ.get("PG_PASSWD")
 
         db_url = f"postgresql://{db_username}:{db_passwd}@{host}/{DB_NAME}"
         self._engine = create_engine(db_url, echo=True)
