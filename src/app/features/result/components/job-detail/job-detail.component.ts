3a4197133a46f235209d568e460def242f43d68d
Lyka <lykawangg@gmail.com>
2024-08-06 13:48:43 -0700

    add some simple styling to the job data

--- src/app/features/result/components/job-detail/job-detail.component.ts
+++ src/app/features/result/components/job-detail/job-detail.component.ts
@@ -5,17 +5,23 @@ import { Job } from '../../../../shared/models/jobs.model';
 @Component({
     selector: 'app-job-detail',
     templateUrl: './job-detail.component.html',
-    styleUrls: ['./job-detail.component.scss']
+    styleUrls: ['./job-detail.component.scss'],
 })
 export class JobDetailComponent implements OnInit {
     @Input() job$: Observable<Job | null> | null = null;
 
     job: Job | null = null;
+    parameterEntries: { key: string; value: string }[] = [];
 
     ngOnInit(): void {
         if (this.job$) {
-            this.job$.subscribe(job => {
+            this.job$.subscribe((job) => {
                 this.job = job;
+                if (job) {
+                    this.parameterEntries = Object.entries(job.parameters || {})
+                        .filter(([key, value]) => value !== null && value !== undefined)
+                        .map(([key, value]) => ({ key, value: value as string }));
+                }
             });
         }
     }
